c52aa73da903d6e4174eeef2663e5bc1163785b1
Joonhong Kim <30318926+JoonHong-Kim@users.noreply.github.com>
2021-12-08 20:53:23 +0900

    add: mask_correlated_samples_2 in AttnGAN+CL
    I think the pretrain_DAMSM.py works reliably only when this function is also at masks.py in the AttnGAN+CL folder.

--- AttnGAN+CL/code/masks.py
+++ AttnGAN+CL/code/masks.py
@@ -7,3 +7,11 @@ def mask_correlated_samples(args):
         mask[i, args.batch_size + i] = 0
         mask[args.batch_size + i, i] = 0
     return mask
+
+def mask_correlated_samples_2(batch_size):
+    mask = torch.ones((batch_size * 2, batch_size * 2), dtype=bool)
+    mask = mask.fill_diagonal_(0)
+    for i in range(batch_size):
+        mask[i, batch_size + i] = 0
+        mask[batch_size + i, i] = 0
+    return mask
